0f0169c47f46a4b2be0428819d0d720becfe4df9
Brandon Jennings <Brandon.Jennings@naturalessentials.local>
2020-09-23 16:57:33 -0400

    Another tutorial completed, styling finished for next tutorial

--- static/JS/script.js
+++ static/JS/script.js
@@ -104,5 +104,55 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
 var all_buttons = document.getElementsByTagName("button");
 var copyAllButtons = [];
 for (let i = 0; i < all_buttons.length; i++) {
-  copyAllButtons.push(all_buttons[i]);
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === "red") {
+    buttonsRed();
+  } else if (buttonThingy.value === "green") {
+    buttonsGreen();
+  } else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  } else if (buttonThingy.value === "random") {
+    randomColors();
+  }
+}
+
+function buttonsRed() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-danger");
+  }
+}
+
+function buttonsGreen() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add("btn-success");
+  }
+}
+
+function buttonColorReset() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(copyAllButtons[i]);
+  }
+}
+
+function randomColors() {
+  let choices = [
+    "btn-primary",
+    "btn-danger",
+    "btn-success",
+    "btn-warning",
+    "btn-secondary",
+    "btn-info",
+    "btn-dark",
+  ];
+  for (let i = 0; i < all_buttons.length; i++) {
+    let randomNumber = Math.floor(Math.random() * 7);
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(choices[randomNumber]);
+  }
 }
